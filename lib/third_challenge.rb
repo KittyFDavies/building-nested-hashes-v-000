06404ec1b8bbba2150a17b7bb47a5ae03ebfaf64
Kitty Davies <kittyfdavies@gmail.com>
2018-03-21 22:16:42 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,22 +13,45 @@ def third_challenge
         :name => "Romeo",
         :age => "15"
         },
-      :hero_friends => []
-   },
+      :hero_friends => [
+        {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
+      ]
+    },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
-        :age => "53"
+        :age => "50"
         },
       :matriarch => {
         :name => "Lady Capulet",
-        :age => "53"
+        :age => "51"
         },
       :heroine => {
         :name => "Juliet",
-        :age => "15"
+        :age => "15",
+        :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
+      ]
    }
   }
 
